8fac0fbc749192ae824a09f5e30ebc10d3a566c5
adrielbueno <adrielbueno28@gmail.com>
2022-09-13 14:48:09 -0300

    feat: funcoes de calculos

--- aula01/index.js
+++ aula01/index.js
@@ -1,2 +1,25 @@
 "use strict";
-console.log('oi');
+//---------------------------1-------------------------------//
+const doisNumeros = [2, 3];
+console.log('Soma de dois números: ' + sum(doisNumeros));
+function sum(numbers) {
+    return numbers[0] + numbers[1];
+}
+//---------------------------2-------------------------------//
+const cincoNumeros = [5, 5, 5, 5, 5];
+console.log('Media de cinco números: ' + average(cincoNumeros));
+function average(numbers) {
+    let avg = 0;
+    numbers.forEach(function (value) {
+        avg += value;
+    });
+    return avg / numbers.length;
+}
+const dadosImc = {
+    altura: 1.82,
+    peso: 90
+};
+console.log('Cálculo IMC: ' + computeImc(dadosImc).toFixed(2));
+function computeImc(dados) {
+    return dados.peso / dados.altura ** 2;
+}
--- aula01/index.ts
+++ aula01/index.ts
@@ -1,1 +1,42 @@
-console.log('oi');
+//---------------------------1-------------------------------//
+
+const doisNumeros: number[] = [2,3];
+
+console.log('Soma de dois números: ' + sum(doisNumeros));
+
+function sum(numbers: number[]): number {
+  return numbers[0] + numbers[1];
+}
+
+//---------------------------2-------------------------------//
+
+const cincoNumeros: number[] = [5,5,5,5,5];
+
+console.log('Media de cinco números: ' + average(cincoNumeros));
+
+function average(numbers: number[]): number {
+  let avg: number = 0;
+  numbers.forEach(function (value) {
+      avg += value;
+  });
+
+  return avg/numbers.length;
+}
+
+//---------------------------3-------------------------------//
+
+type Pessoa = {
+  altura: number,
+  peso: number;
+};
+
+const dadosImc: Pessoa = {
+  altura : 1.82,
+  peso : 90
+};
+
+console.log('Cálculo IMC: ' + computeImc(dadosImc).toFixed(2));
+
+function computeImc(dados: Pessoa): number {
+  return dados.peso/dados.altura**2;
+}
